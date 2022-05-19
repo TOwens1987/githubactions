6b244573b303437d17148bc9649de97ef71af7a9
Thomas Owens <Thomas.Owens2@va.gov>
2022-05-19 17:04:42 -0400

    test  BREAKING CHANGE:  BREAKING CHANGE:

--- pre-commit.sh
+++ pre-commit.sh
@@ -10,12 +10,10 @@ sed -i.bak -e "1s/^/feat(): /" $1
 elif [ "$BRANCH_NAME" = "patch/" ] || [ "$BRANCH_NAME" = "hotfix/" ] || [ "$BRANCH_NAME" = "bug/" ]; then
 sed -i.bak -e "1s/^/fix(): /" $1
 elif [ "$BRANCH_NAME" = "breaking/" ]; then
-sed -i.bak -e "1s/$/\n BREAKING CHANGE: /" $1
+sed -i.bak -e "1s/^/perf(): /" $1 | sed -i.bak -e "1s/$/\n BREAKING CHANGE: /" $1
 else
         echo "Aborting commit due to improper commit message structure."
         exit 1
 fi
 
 EOF
-
-sed -i.bak -e "1s/$/[+semver: patch] /" $1
